96b420713eec6ad3b7dac843f84abcae571739b9
Mirko Mälicke <mirko@hydrocode.de>
2022-12-26 20:33:14 +0100

    add static index

--- camels_serv/api/static.py
+++ camels_serv/api/static.py
@@ -7,6 +7,20 @@ from camels_serv.core import config
 
 static_blueprint = Blueprint('static', __name__)
 
+
+@static_blueprint.route('/', methods=['GET'])
+def index():
+    return jsonify({
+        'message': 'CAMELS-DE API - static data API',
+        'endpoints': [
+            {
+                'url': '/static/germany.geojson',
+                'description': 'Return the admisnistrative boundary level 2 of Germany, extracted from OpenStreetMap as GeoJSON',
+                'methods': ['GET']
+            },
+        ]
+    })
+
 @static_blueprint.route('/<string:name>', methods=['GET'])
 def get_static_file(name: str):
     # build the file name
