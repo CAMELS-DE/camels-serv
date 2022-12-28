45540356f1b7410d5d44804e44be170ed8f3381c
Mirko Mälicke <mirko@hydrocode.de>
2022-12-28 09:21:43 +0100

    implement a base loader

--- camels_serv/core/processState.py
+++ camels_serv/core/processState.py
@@ -1,39 +1,11 @@
 import os
 
-import pandas as pd
 import geopandas as gpd
-import fiona
 
-from camels_serv.core import config
-
-
-class ProcessState:
-    def __init__(self, output_dir: str = config.BASEPATH) -> None:
-        """
-        Process State instance to describe current state of CAMELS-DE processing.
-        """
-        # save the base path
-        self.base_path = os.path.abspath(output_dir)
-
-        # derive all paths from the BASEPATH
-        self.metadata_path = os.path.join(self.base_path, 'metadata')
-
-        
-        # automatically load metadata
-        self.metadata = self.load_metadata()
-
-    def load_metadata(self) -> gpd.GeoDataFrame:
-        """
-        Load metadata and transform to geodata
-        """
-        # build the fname
-        fname = os.path.join(self.metadata_path, config.METADATA_FILE_NAME)
-        df = pd.read_csv(fname)
-        gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.x, df.y), crs=3035).to_crs(epsg=4326)
-        
-        return gdf
+from camels_serv.core.loader import BaseLoader
 
 
+class ProcessState(BaseLoader):
     def has_output(self, base_df: gpd.GeoDataFrame = None) -> gpd.GeoDataFrame:
         """
         Return the pegel GeoDataFrame enhanced with info about existing output
